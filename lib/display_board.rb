b7e58a4faffbf5e61f1cb964efa37be692ba8122
Robert Wayne <robertjwayne@ucla.edu>
2017-04-04 19:47:30 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,35 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+
+  # Print first row
+  print " #{board[0]} "
+  print "|"
+  print " #{board[1]} "
+  print "|"
+  print " #{board[2]} "
+  puts
+
+  # Print first divider
+  puts "-----------"
+
+  # Print second row
+  print " #{board[3]} "
+  print "|"
+  print " #{board[4]} "
+  print "|"
+  print " #{board[5]} "
+  puts
+
+  # Print second divider
+  puts "-----------"
+
+  # Print third row
+  print " #{board[6]} "
+  print "|"
+  print " #{board[7]} "
+  print "|"
+  print " #{board[8]} "
+  puts
+
+end
